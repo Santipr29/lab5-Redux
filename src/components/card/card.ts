5f090622e7b5aa1c9097eb0c2c0d356b77f7bdbe
Santiago <santipr2004@gmail.com>
2023-04-13 15:02:50 -0500

    feat:component

---
+++ src/components/card/card.ts
@@ -0,0 +1,67 @@
+import styles from "./card.css"
+
+export enum Attributes {
+    "name" = "name",
+    "birth_year" = "birth_year",
+    "gender" = "gender"
+}
+
+class MyCard extends HTMLElement {
+    name?: string;
+    birth_year?: string;
+    gender?: string
+
+    static get observedAttributes() {
+        const attrs: Record<Attributes, null> = {
+            birth_year: null,
+            name: null,
+            gender: null,
+        };
+        return Object.keys(attrs);
+    }
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    attributeChangedCallback(
+        propName: Attributes,
+        _: string | undefined,
+        newValue: string | undefined
+        ) {
+            switch (propName) {
+
+                default:
+                this[propName] = newValue;
+                break;
+            }
+
+            this.render();
+        }
+
+        render() {
+            if (this.shadowRoot) {
+                this.shadowRoot.innerHTML = ``
+
+                const css = this.ownerDocument.createElement("style");
+                css.innerHTML = styles;
+                this.shadowRoot?.appendChild(css);
+
+                this.shadowRoot.innerHTML += `
+                <section class="cardSection">
+                    <h1 class="name">Name: ${this.name}</h1>
+                    <p class="birthYear">Birth Year: ${this.birth_year}</p>
+                    <p class="birthYear">Gender: ${this.gender}</p>
+                </section>
+                `;
+            }
+        }
+    }
+
+customElements.define("my-card", MyCard);
+export default MyCard;
---
+++ src/components/export.ts
@@ -0,0 +1,1 @@
+export {default as MyCard} from "./card/card"
